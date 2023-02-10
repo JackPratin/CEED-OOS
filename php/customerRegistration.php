a7ebae280515b6f279535d2ee5fd98abe3d41227
Jan Patrick Edisan <janpatrickedisan0407@gmail.com>
2023-02-10 22:24:02 +0800

    customer registration

--- php/customerRegistration.php
+++ php/customerRegistration.php
@@ -10,7 +10,13 @@
     $address    = mysqli_real_escape_string($con, $_POST['address']);
     $brgy       = mysqli_real_escape_string($con, $_POST['brgy']);
     $city       = mysqli_real_escape_string($con, $_POST['city']);
+    $username   = mysqli_real_escape_string($con, $_POST['username']);
   
     mysqli_query($con, "INSERT INTO `customer_tb`( `first_name`, `middle_initial`, `last_name`, `contact_number`, `email`, `order_count`) VALUES ('$fname','$mi','$lname','$contact','$email','0')");
 
+    mysqli_query($con, "INSERT INTO `customer_tb`(`first_name`, `middle_initial`, `last_name`, `contact_number`, `email`, `order_count`, `address`, `baranggay`, `city`, `username`, `password`) VALUES ('$fname','$mi','$lname','$contact','$email','0','$address','$brgy','$city','$username', $password)");
+
+    
+
+    echo '<script>window.location="../index.php"</script>';
 ?>
