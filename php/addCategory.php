f484a926a8a239d927056149427cf983ecc2bdb3
J.P. Dasine <52884434+JackPratin@users.noreply.github.com>
2023-04-15 08:45:16 +0800

    Create addCategory.php
    addition of category

---
+++ php/addCategory.php
@@ -0,0 +1,11 @@
+<?php
+    require("config.php");
+    
+    $category      = mysqli_real_escape_string($con, $_POST['category']);
+    );
+
+    $insertqry = mysqli_query($con, "INSERT INTO `product_categories_tb`(`category_name`) VALUES ('$category')");
+
+    echo "<script>alert('Category Added.');</script>";
+    echo '<script>window.location="../admin-product.php"</script>';
+?>
