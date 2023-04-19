293efa3dd165d3448a80f62ac9e8b9f0f161800b
Jan Patrick Edisan <janpatrickedisan0407@gmail.com>
2023-04-19 13:04:03 +0800

    registration validation

--- js/registrationCheck.js
+++ js/registrationCheck.js
@@ -8,18 +8,28 @@ $('document').ready(function(){
          return;
      }
      $.ajax({
-       url: 'register.php',
+       url: '../../CEED-OOS/php/customerRegistration.php',
        type: 'post',
        data: {
            'username_check' : 1,
            'username' : username,
        },
        success: function(response){
+        if(document.getElementById("usernameSpan") == null){
+            const span = document.createElement("span");
+            const br = document.createElement("br");
+            span.setAttribute('id','usernameSpan');
+            const element = document.getElementById("usernameDiv");
+            // const child = document.getElementById("password");
+            // const child1 = document.getElementById("usernameSpan");
+            // element.appendChild(br);
+            element.appendChild(span);
+        }
          if (response == 'taken' ) {
              username_state = false;
              $('#username').parent().removeClass();
              $('#username').parent().addClass("form_error");
-             $('#username').siblings("span").text('Sorry... Username already taken');
+             $('#username').siblings("span").text('Username is already registered to an account.');
          }else if (response == 'not_taken') {
              username_state = true;
              $('#username').parent().removeClass();
@@ -36,52 +46,123 @@ $('document').ready(function(){
             return;
         }
         $.ajax({
-         url: 'register.php',
+         url: '../../CEED-OOS/php/customerRegistration.php',
          type: 'post',
          data: {
              'email_check' : 1,
              'email' : email,
          },
          success: function(response){
+            if(document.getElementById("emailSpan") == null){
+                const span = document.createElement("span");
+                const br = document.createElement("br");
+                span.setAttribute('id','emailSpan');
+                const element = document.getElementById("emailDiv");
+                const child = document.getElementById("contact");
+                // const child1 = document.getElementById("usernameSpan");
+                element.insertBefore(span, child);
+                element.insertBefore(br, child);
+            }
+            
              if (response == 'taken' ) {
-             email_state = false;
-             $('#email').parent().removeClass();
-             $('#email').parent().addClass("form_error");
-             $('#email').siblings("span").text('Sorry... Email already taken');
-             }else if (response == 'not_taken') {
-               email_state = true;
-               $('#email').parent().removeClass();
-               $('#email').parent().addClass("form_success");
-               $('#email').siblings("span").text('Email available');
+                email_state = false;
+                $('#email').parent().removeClass();
+                $('#email').parent().addClass("form_error");
+                $('#email').siblings("span").text('Email is already registered to an account.');
+             }
+             else if (response == 'not_taken') {
+                email_state = true;
+                $('#email').parent().removeClass();
+                $('#email').parent().addClass("form_success");
+                $('#email').siblings("span").text('Email available.');  
              }
          }
         });
     });
+
+        $('#conPassword').on('blur', function(){
+            var password = $('#password').val();
+            var conPassword = $('#conPassword').val();
+            if(password != conPassword){
+                if(document.getElementById("passwordSpan") == null){
+                    const span = document.createElement("span");
+                    const br = document.createElement("br");
+                    span.setAttribute('id','passwordSpan');
+                    const element = document.getElementById("passwordDiv");
+                    // const child1 = document.getElementById("usernameSpan");
+                    element.appendChild(br);
+                    element.appendChild(span);
+
+                    $('#passwordSpan').parent().removeClass();
+                    $('#passwordSpan').parent().addClass("form_error");
+                    $('#passwordSpan').text('Password does not match.');
+                    
+                }
+            }
+            else{
+                if(document.getElementById("passwordSpan") != null){
+                    const element = document.getElementById("passwordSpan");
+                    element.remove(); 
+                }
+                
+                $('#passwordDiv').removeClass();
+                $('#passwordDiv').addClass("form_success");
+            }
+        });
    
-    $('#reg_btn').on('click', function(){
-        var username = $('#username').val();
-        var email = $('#email').val();
-        var password = $('#password').val();
-        if (username_state == false || email_state == false) {
-         $('#error_msg').text('Fix the errors in the form first');
-       }else{
-         // proceed with form submission
-         $.ajax({
-             url: 'register.php',
-             type: 'post',
-             data: {
-                 'save' : 1,
-                 'email' : email,
-                 'username' : username,
-                 'password' : password,
-             },
-             success: function(response){
-                 alert('user saved');
-                 $('#username').val('');
-                 $('#email').val('');
-                 $('#password').val('');
-             }
-         });
-        }
-    });
+    // $('#regSubmit2').on('click', function(){
+    //     var username = $('#username').val();
+    //     var email = $('#email').val();
+    //     var password = $('#password').val();
+    //     var conPassword = $('#conPassword').val();
+    //     var password = $('#password').val();
+    //     var password = $('#password').val();
+    //     var password = $('#password').val();
+    //     var password = $('#password').val();
+    //     var password = $('#password').val();
+    //     var password = $('#password').val();
+    //     var password = $('#password').val();
+    //     if(password != conPassword){
+            
+    //     }
+    //     else{
+    //         if (username_state == false || email_state == false) {
+    //             $('#error_msg').text('Fix the errors in the form first');
+    //           }
+    //           else{
+    //             // proceed with form submission
+    //             $.ajax({
+    //                 url: '../../CEED-OOS/php/customerRegistration.php',
+    //                 type: 'post',
+    //                 data: {
+    //                     'save' : 1,
+    //                     'email' : email,
+    //                     'username' : username,
+    //                     'fname' : fname,
+    //                     'mi' : mi,
+    //                     'lname' : lname,
+    //                     'contact' : contact,
+    //                     'address' : address,
+    //                     'brgy' : brgy,
+    //                     'city' : city,
+    //                     'password' : password,
+    //                 },
+    //                 success: function(response){
+    //                     alert('user saved');
+    //                     $('#username').val('');
+    //                     $('#email').val('');
+    //                     $('#fname').val('');
+    //                     $('#mi').val('');
+    //                     $('#lname').val('');
+    //                     $('#contact').val('');
+    //                     $('#address').val('');
+    //                     $('#brgy').val('');
+    //                     $('#city').val('');
+    //                     $('#password').val('');
+    //                 }
+    //             });
+    //         }
+    //     }
+        
+    // });
    });
--- php/customerRegistration.php
+++ php/customerRegistration.php
@@ -1,23 +1,63 @@
 <?php
     require("config.php");
 
-    $fname      = mysqli_real_escape_string($con, $_POST['fname']);
-    $lname      = mysqli_real_escape_string($con, $_POST['lname']);
-    $mi         = mysqli_real_escape_string($con, $_POST['mi']);
-    $contact    = mysqli_real_escape_string($con, $_POST['contact']);
-    $email      = mysqli_real_escape_string($con, $_POST['email']);
-    $password   = mysqli_real_escape_string($con, $_POST['password']);
-    $address    = mysqli_real_escape_string($con, $_POST['address']);
-    $brgy       = mysqli_real_escape_string($con, $_POST['brgy']);
-    $city       = mysqli_real_escape_string($con, $_POST['city']);
-    $username   = mysqli_real_escape_string($con, $_POST['username']);
-  
-    $hash_password = hash('sha256', $password);
+    if (isset($_POST['username_check'])) {
+        
+        $username   = mysqli_real_escape_string($con, $_POST['username']);
+        $results = mysqli_query($con, "SELECT * FROM `customer_tb` WHERE username='$username'");
+        if (mysqli_num_rows($results) > 0) {
+          echo "taken";	
+        }else{
+          echo 'not_taken';
+        }
+        exit();
+    }
+    if (isset($_POST['email_check'])) {
+        
+        $email      = mysqli_real_escape_string($con, $_POST['email']);
+        $results = mysqli_query($con, "SELECT * FROM `customer_tb` WHERE email='$email'");
+        if (mysqli_num_rows($results) > 0) {
+          echo "taken";	
+        }else{
+          echo 'not_taken';
+        }
+        exit();
+    }
+    if (isset($_POST['email'])) {
+        $fname      = mysqli_real_escape_string($con, $_POST['fname']);
+        $lname      = mysqli_real_escape_string($con, $_POST['lname']);
+        $mi         = mysqli_real_escape_string($con, $_POST['mi']);
+        $contact    = mysqli_real_escape_string($con, $_POST['contact']);
+        $email      = mysqli_real_escape_string($con, $_POST['email']);
+        $password   = mysqli_real_escape_string($con, $_POST['password']);
+        $address    = mysqli_real_escape_string($con, $_POST['address']);
+        $brgy       = mysqli_real_escape_string($con, $_POST['brgy']);
+        $city       = mysqli_real_escape_string($con, $_POST['city']);
+        $username   = mysqli_real_escape_string($con, $_POST['username']);
+    
+        $hash_password = hash('sha256', $password);
 
-    mysqli_query($con, "INSERT INTO `customer_tb`(`first_name`, `middle_initial`, `last_name`, `contact_number`, `email`, `order_count`, `address`, `baranggay`, `city`, `username`, `password`) VALUES ('$fname','$mi','$lname','$contact','$email','1','$address','$brgy','$city','$username', '$hash_password'
-    )");
+        $sql = "SELECT * FROM customer_tb WHERE username='$username'";
+        $results = mysqli_query($con, $sql);
+        if (mysqli_num_rows($results) > 0) {
+          echo "exists";	
+          exit();
+        }else{
+            
+
+            mysqli_query($con, "INSERT INTO `customer_tb`(`first_name`, `middle_initial`, `last_name`, `contact_number`, `email`, `order_count`, `address`, `baranggay`, `city`, `username`, `password`) VALUES ('$fname','$mi','$lname','$contact','$email','1','$address','$brgy','$city','$username', '$hash_password')");
+
+            // echo 'Saved!';
+            echo "<script>alert('Account created.');</script>";
+            echo '<script>window.location="../index.php"</script>';
+            exit();
+        }
+    }
+
+
+    // mysqli_query($con, "INSERT INTO `customer_tb`(`first_name`, `middle_initial`, `last_name`, `contact_number`, `email`, `order_count`, `address`, `baranggay`, `city`, `username`, `password`) VALUES ('$fname','$mi','$lname','$contact','$email','1','$address','$brgy','$city','$username', '$hash_password'
+    // )");
 
     
-    echo "<script>alert('Account created.');</script>";
-    echo '<script>window.location="../index.php"</script>';
+    
 ?>
